505f58f3ae170be9af1af9a9ad636052f5785c97
andersontbernal <andersonbernal7@gmail.com>
2025-07-11 11:03:19 +0000

    Poryecto semaforo completado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';  //defino la funcion useState para guardar informacion y que esta sea dinamica segun lo que quiero mostrar
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Home = () => {                      // home lo creo para renderizar (mostrar)
+	const [color, setColor] = useState(""); //"color" como variable para guardar las variables de las luces y setClor para definirla como funcion. 
 
-//create your first component
-const Home = () => {
+	const lightStyle = (lightColor) => ({
+		width: "80px",
+		height: "80px",
+		backgroundColor: lightColor,
+		borderRadius: "50%",
+		margin: "10px auto",
+		boxShadow: color === lightColor ? `0 0 20px 5px ${lightColor}` : "none",
+		opacity: color === lightColor ? 1 : 0.3, // 1 parametro para decir que es visible y 0.3 un poco mas oscura
+		transition: "all 0.3s ease", //cambio de color y brillo  suave
+		cursor: "pointer",
+	});
+
+	const containerStyle = {
+		backgroundColor: "black",
+		padding: "20px",
+		borderRadius: "20px",
+		width: "120px",
+		margin: "100px auto",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	};
+
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div style={containerStyle}>
+				<div style={lightStyle("red")} onClick={() => setColor("red")}></div>
+				<div style={lightStyle("yellow")} onClick={() => setColor("yellow")}></div>
+				<div style={lightStyle("green")} onClick={() => setColor("green")}></div>
+			</div>
 		</div>
 	);
 };
